7636378149338da1bcdd00c2a0ad4ffd7aa76dc3
Fabian Ziebig <ziebig@bigmind.de>
2019-03-21 21:33:35 +0100

    Hello Vue!

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ var app = new Vue({
 });
 
 app.message = 'Hallo Vue!';
+
+
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        message: 'You loaded this page on ' + new Date().toLocaleString()
+    }
+});
