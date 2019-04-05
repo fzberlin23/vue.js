57f1b92280570e49407eb00773e2d2848b023131
Fabian Ziebig <ziebig@bigmind.de>
2019-04-05 08:36:31 +0200

    Hello Vue!

--- index.js
+++ index.js
@@ -197,3 +197,14 @@ new Vue({
     }
   }
 })
+
+// Define a new component called button-counter
+Vue.component('button-counter', {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+})
+new Vue({ el: '#components-demo' })
